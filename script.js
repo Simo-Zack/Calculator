323d34c426e58beb7ed5d99408b31c5170e109de
Simo-Zack <simozack2009@gmail.com>
2026-08-04 15:39:33 +0200

    Prime funzioni funzionanti create. Costanti per la logica del caloclo Funzioni fatte: numero, operatore, cancella

--- script.js
+++ script.js
@@ -1,27 +1,93 @@
 const display = document.getElementById("display");
-
-
+const calcolo = {
+    "num1": null,
+    "operazione": null,
+    "num2": null
+};
 
 const operatori = document.querySelectorAll(".operatore");
-operatori.forEach(el => {
-    el.addEventListener("click", operatore);
+operatori.forEach(op => {
+    op.addEventListener("click", operatore);
 });
 
 const numeri = document.querySelectorAll(".nero");
-numeri.forEach(el => {
-    el.addEventListener("click", numero);
+numeri.forEach(num => {
+    num.addEventListener("click", numero);
 });
 
-function operatore(e) {
-    
+function numero(e) {
+    let num = e.target.innerText;
+    if (calcolo.num1 === null) {
+        let primo = document.createElement("span");
+        primo.id = "primo";
+        primo.innerText = num;
+        display.appendChild(primo);
+        calcolo.num1 = num;
+    } else if (calcolo.num1 !== null && calcolo.operazione === null) {
+        document.getElementById("primo").innerText += num;
+        calcolo.num1 += num;
+    } else if (calcolo.operazione !== null && calcolo.num2 === null) {
+        let secondo = document.createElement("span");
+        secondo.id = "secondo";
+        secondo.innerText = num;
+        display.appendChild(secondo);
+        calcolo.num2 = num;
+    } else {
+        document.getElementById("secondo").innerText += num;
+        calcolo.num2 += num;
+    }
 }
 
-function numero(e) {
-
+function operatore(e) {
+    let op = e.target.innerText;
+    if (calcolo.num1 === null) {
+        return;
+    } else if (calcolo.num1 !== null && calcolo.operazione === null && calcolo.num2 === null) {
+        let operaz = document.createElement("span");
+        operaz.id = "operaz";
+        operaz.innerText = op;
+        display.appendChild(operaz);
+        calcolo.operazione = op;
+    } else if (calcolo.num1 !== null && calcolo.operazione !== null && calcolo.num2 === null) {
+        document.getElementById("operaz").innerText = op;
+        calcolo.operazione = op;
+    } /*else {
+        uguale();
+        let operaz = document.createElement("span");
+        operaz.id = "operaz";
+        operaz.innerText = op;
+        display.appendChild(operaz);
+        calcolo.operazione = op;
+    }*/
 }
 
 function cancella() {
-
+    if (calcolo.num1 === null) {
+        return;
+    } else if (calcolo.num !== null && calcolo.operazione === null) {
+        let num = document.getElementById("primo").innerText;
+        num = num.slice(0, -1);
+        if (num.length === 0) {
+            document.getElementById("primo").remove();
+            calcolo.num1 = null;
+        } else {
+            document.getElementById("primo").innerText = num;
+            calcolo.num1 = num;
+        }
+    } else if (calcolo.operazione !== null && calcolo.num2 === null) {
+        document.getElementById("operaz").remove();
+        calcolo.operazione = null;
+    } else {
+        let num = document.getElementById("secondo").innerText;
+        num = num.slice(0, -1);
+        if (num.length === 0) {
+            document.getElementById("secondo").remove();
+            calcolo.num2 = null;
+        } else {
+            document.getElementById("secondo").innerText = num;
+            calcolo.num2 = num;
+        }
+    }
 }
 
 function elimina() {
@@ -29,6 +95,10 @@ function elimina() {
 }
 
 function percentuale() {
+
+}
+
+function cambioSegno() {
 
 }
 
